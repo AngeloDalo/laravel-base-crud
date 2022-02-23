bb316cc054de84469ccef3de10ee7d85d0be57d3
AngeloDalo <angelo251158@gmail.com>
2022-02-23 13:42:15 +0100

    column

--- laravel/app/Comic.php
+++ laravel/app/Comic.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'editor',
+        'writer',
+        'description',
+        'price',
+        'photo',
+        'page',
+        '+18',
+        'created_at',
+        'updated_at',
+    ];
 }
